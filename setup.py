4b950c1f0dfc0cb982ae1c74d9c94ac86bf3480c
andre.cintra <andre.cintra@zup.com.br>
2023-01-16 14:12:02 -0300

    feat: making it a python lib

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="pluginschema",
+    version="0.1.0",
+    description="",
+    long_description=long_description,
+    url="https://github.com/stack-spot/schemastore-stackspot",
+    license="MIT",
+    author="andrecintra",
+    author_email="andre.cintra@zup.com.br",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    install_requires=[],
+)
