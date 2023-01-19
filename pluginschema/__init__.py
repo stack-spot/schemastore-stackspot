7c4f58412536e4571a12df1462c71c91ace796e2
andre.cintra <andre.cintra@zup.com.br>
2023-01-19 10:15:09 -0300

    bug: Attempt to fix import template in action

--- pluginschema/__init__.py
+++ pluginschema/__init__.py
@@ -9,15 +9,7 @@ class TypeEnum(Enum) :
     INFRA = "infra"
 
 def get_template(type_enum: TypeEnum, schema_version: str = "v1"):
-        if getattr(sys, "frozen", False):
-            template_path = Path(sys._MEIPASS) / type_enum.value / schema_version
-            if sys.platform != "win32":
-                chmod(template_path / "templates" / "templates" / "README.md", 0o644)
-                chmod(template_path / "templates" / f"plugin-{type_enum.value}.stk.yaml", 0o644)
-        else:
-            template_path = (
-                Path(__file__).parent / type_enum.value / schema_version
-            )
-        return template_path
+        return Path(__file__).parent / type_enum.value / schema_version
+            
 
     
