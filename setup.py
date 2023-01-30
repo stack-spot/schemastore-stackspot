6ef0b7f8f176a4e3b205991b362cd1c8fba2d593
andre.cintra <andre.cintra@zup.com.br>
2023-01-30 11:35:54 -0300

    chore: bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="pluginschema",
-    version="0.1.1",
+    version="0.1.2",
     description="",
     long_description=long_description,
     url="https://github.com/stack-spot/schemastore-stackspot",
